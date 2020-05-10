89612a43272d5d4e3f028d1e80e11eb82235c1fe
d03 <15378943+d03@users.noreply.github.com>
2020-05-11 01:45:29 +0200

    edit assignment 1 finished

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from math import log2
+from functools import reduce
+
+
 class txt:
     def __init__(self, path: str):
         self.__data: list = self.__load_txt(path)
@@ -24,12 +28,52 @@ class txt:
         return d1, d2
 
 
+def huffman(p: dict, inverted=False) -> (dict, float, float):
+    temp: dict = p
+    code: dict = dict(map(lambda k: (k, ''), temp))
+
+    # assemble huffman tree and generate code
+    tree = ()
+    while len(p) > 1:
+        p = {k: v for k, v in sorted(p.items(), key=lambda item: item[1])}
+
+        def get_min_val_item(d: dict) -> (int, str):
+            return min(d, key=lambda x: d[x]), d[min(d, key=lambda x: d[x])]
+
+        left = get_min_val_item(p)
+        p.pop(left[0])
+        right = get_min_val_item(p)
+        p.pop(right[0])
+        p[left[0] + right[0]] = left[1] + right[1]
+        tree = (tree, (left[0], right[0]))
+        for k in temp:
+            if k in left[0]:
+                if not inverted:
+                    code[k] += '0'
+                else:
+                    code[k] += '1'
+            if k in right[0]:
+                if not inverted:
+                    code[k] += '1'
+                else:
+                    code[k] += '0'
+
+    # compute the entropy & the mean length
+    entropy: float = reduce(lambda x, y: x + y, map(lambda x: (-1) * log2(temp[x]) * temp[x], temp))
+    mean_length: float = reduce(lambda x, y: x + y, map(lambda x: len(code[x]) * temp[x], temp))
+    return code, entropy, mean_length
+
+
 def main() -> None:
+    print("Test:")
+    list(map(print, huffman({"A": 0.05, "B": 0.03, "C": 0.17, "D": 0.23, "E": 0.01, "F": 0.32, "G": 0.19})))
+    print("Midsummer:")
     buf: txt = txt("midsummer.txt")
     print(len(buf))
     d1, d2 = buf.count_char_occurrences()
     print(d1)
     print(d2)
+    list(map(print, huffman(d2)))
     return
 
 
